665d10ab0d286e7c44db6da3ef1d035526d79fe3
chthollys <jason130105@gmail.com>
2025-07-18 13:35:05 +0700

    config AWS remote storage
    Signed-off-by: chthollys <jason130105@gmail.com>

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,16 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: `${process.env.AWS_S3_BUCKET_NAME}.${process.env.AWS_S3_REGION}.s3.amazonaws.com`,
+        port: "",
+        pathname: "/**",
+      },
+    ],
+  },
   experimental: {
     serverActions: {
       bodySizeLimit: "3mb",
